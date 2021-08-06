9a68139115d6d8a9bfcba588f442929ceed36e0f
jaeho choi <chlwogh1993@gmail.com>
2021-08-06 15:51:25 +0900

    commit

---
+++ src/views/VueChartJS.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="container">
+    <h1>Demo examples of vue-chartjs</h1>
+    <div class="columns">
+      <div class="column">
+        <h3>Line Chart</h3>
+        <line-chart></line-chart>
+      </div>
+      <div class="column">
+        <h3>Bar Chart</h3>
+        <bar-chart></bar-chart>
+        <!--Bar Chart example-->
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <h3>Bubble Chart</h3>
+        <bubble-chart></bubble-chart>
+        <!--Bubble Chart example-->
+      </div>
+      <div class="column">
+        <h3>Reactivity - Live update upon change in datasets</h3>
+        <!--Reactivity Line Chart example-->
+        <reactive></reactive>
+      </div>
+    
+    </div>
+  </section>
+</template>
+ 
+<script>
+  import LineChart from '@/components/LineChart'
+  import BarChart from '@/components/BarChart'
+  import BubbleChart from '@/components/BubbleChart'
+  import Reactive from '@/components/Reactive'
+ 
+  export default {
+    name: 'VueChartJS',
+    components: {
+      LineChart,
+      BarChart,
+      BubbleChart,
+      Reactive
+    },
+ 
+  }
+</script>
+ 
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+ 
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+ 
+  a {
+    color: #42b983;
+  }
+</style>
+ 
